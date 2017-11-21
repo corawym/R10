8c307f0b61ee520bd9c692af7972cd3fe2335ee6
corawym <corawym@gmail.com>
2017-11-21 01:27:31 -0800

    Rough out session scene

---
+++ js/scenes/Session/Session.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+
+import { styles } from './styles';
+
+const Session = () => {
+  console.log(data)
+  return(
+    <Text>Session scene</Text>
+  )
+}
+
+Session.propTypes = {
+
+}
+
+export default Session;
---
+++ js/scenes/Session/SessionContainer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import PropTypes from 'prop-types';
+
+import Session from './Session';
+
+class SessionContainer extends Component {
+
+  constructor() {
+  }
+
+  static propTypes = {
+
+  }
+
+  render(){
+    <Session/>
+  }
+}
+
+export default SessionContainer;
