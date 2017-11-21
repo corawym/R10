33cf22024f59559d83e80802a8693f6636486cf0
corawym <corawym@gmail.com>
2017-11-21 01:30:30 -0800

    Rough out speaker scene

---
+++ js/scenes/Speaker/Speaker.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+
+import { styles } from './styles';
+
+const Speaker = () => {
+  console.log(data)
+  return(
+    <Text>Speaker scene</Text>
+  )
+}
+
+Speaker.propTypes = {
+
+}
+
+export default Speaker;
---
+++ js/scenes/Speaker/SpeakerContainer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import PropTypes from 'prop-types';
+
+import Speaker from './Speaker';
+
+class SpeakerContainer extends Component {
+
+  constructor() {
+  }
+
+  static propTypes = {
+
+  }
+
+  render(){
+    <Speaker/>
+  }
+}
+
+export default SpeakerContainer;
