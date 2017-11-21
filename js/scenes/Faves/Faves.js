e3630d2571cc53aacb59eeb7cac63342d3aee6d3
corawym <corawym@gmail.com>
2017-11-21 01:33:02 -0800

    Rough out faves scene

---
+++ js/scenes/Faves/Faves.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import PropTypes from 'prop-types';
+
+import { styles } from './styles';
+
+const Faves = () => {
+  console.log(data)
+  return(
+    <Text>Faves scene</Text>
+  )
+}
+
+Faves.propTypes = {
+
+}
+
+export default Faves;
---
+++ js/scenes/Faves/FavesContainer.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import PropTypes from 'prop-types';
+
+import Faves from './Faves';
+
+class FavesContainer extends Component {
+
+  constructor() {
+  }
+
+  static propTypes = {
+
+  }
+
+  render(){
+    <Faves/>
+  }
+}
+
+export default FavesContainer;
