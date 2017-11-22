6c815fd0f38a5564ee594b4ebccbbf1ab1fa3a57
corawym <corawym@gmail.com>
2017-11-22 10:40:10 -0800

    Write the chnages to objects in realm - creating, querying and deleting

--- js/config/models.js
+++ js/config/models.js
@@ -9,4 +9,32 @@ const FaveSchema = {
   }
 };
 
-export default new Realm({schema: [FaveSchema]});
+const realm = new Realm({schema: [FaveSchema]});
+
+export const queryFaves = () => {
+  return realm.objects('Fave');
+}
+
+export const deleteFaves = (faveId) => {
+  let fave = realm.objects('Fave').filter("id == $0", faveId)
+  try {
+    realm.write(() => { 
+      realm.delete(fave); 
+    })
+  } catch (e) {
+    console.log ("Error on deleting");
+  }
+}
+
+export const createFaves = (session_id) => {
+  try{
+    realm.write(() => {
+      realm.create('Fave', {id: 'session_id', faved_on: new Date()});
+    });
+  } catch (e) {
+    console.log ("Error on creation");
+  }  
+}
+
+export default realm;
+
